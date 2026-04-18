ea0a8ab40c4b0015cf8a2b23beeae55f3d010a6d
unknown <izazsqputra@gmail.com>
2026-04-18 13:04:49 +0700

    feat: Animate Art Page with Masked Image

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,15 +1,44 @@
 import React from "react";
-import { featureLists, goodLists } from "../../constant";
+import { featureLists, goodLists } from "../../constant/index.js";
 import { useMediaQuery } from "react-responsive";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const Art = () => {
-    const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
-    useGSAP(() => {
-        
-    })
+  useGSAP(() => {
+    const start = isMobile ? "top 20%" : "top top";
+
+    const maskTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#art",
+        start,
+        end: "bottom center",
+        scrub: 1.5,
+        pin: true,
+      },
+    });
+
+    maskTimeline
+      .to(".will-fade", {
+        opacity: 0,
+        stagger: 0.2,
+        ease: "power1.inOut",
+      })
+      .to(".masked-img", {
+        scale: 1.3,
+        maskPosition: "center",
+        maskSize: "400%",
+        duration: 8,
+        ease: "power1.inOut",
+      })
+      .to("#masked-content", {
+        opacity: 1,
+        duration: 1,
+        ease: "power1.inOut",
+      });
+  });
 
   return (
     <div id="art">
@@ -48,7 +77,10 @@ const Art = () => {
           <h2 className="will-fade">Sip-Worthy Perfection</h2>
           <div id="masked-content">
             <h3>Made with Craft, Poured with Passion</h3>
-            <p>This isn't just a drink. It's a carefully crafted moment made just for you.</p>
+            <p>
+              This isn't just a drink. It's a carefully crafted moment made just
+              for you.
+            </p>
           </div>
         </div>
       </div>
