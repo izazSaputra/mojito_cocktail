4566cf7c6915886cbaf58cb38aacb46579557c3c
unknown <izazsqputra@gmail.com>
2026-04-18 12:46:29 +0700

    feat: Adding Art Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import Navbar from "./components/NavigationBar.jsx";
 import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +17,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 };
